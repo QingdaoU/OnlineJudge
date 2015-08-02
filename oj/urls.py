cf4ae5aa124954162cc24e759a0845157c28d3c2
virusdefender <1670873886@qq.com>
2015-08-02 16:58:41 +0800

    增加题目列表页和部分 ajax js [CI SKIP]

--- oj/urls.py
+++ oj/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     url(r'^problem/(?P<problem_id>\d+)/$', "problem.views.problem_page", name="problem_page"),
 
     url(r'^admin/contest/$', TemplateView.as_view(template_name="admin/contest/add_contest.html"), name="add_contest_page"),
-
+    url(r'^problems/$', TemplateView.as_view(template_name="oj/problem/problem_list.html"), name="problem_list_page"),
 ]
