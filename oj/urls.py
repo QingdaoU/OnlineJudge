593690b340ee1339b53707ebda641b519197abb9
virusdefender <1670873886@qq.com>
2015-08-02 09:59:13 +0800

    前台模板模块化拆分

--- oj/urls.py
+++ oj/urls.py
@@ -6,14 +6,14 @@ from django.views.generic import TemplateView
 from account.views import UserLoginAPIView
 
 urlpatterns = [
-    url("^$", TemplateView.as_view(template_name="utils/index.html"), name="index_page"),
+    url("^$", TemplateView.as_view(template_name="oj/index.html"), name="index_page"),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^admin/$', TemplateView.as_view(template_name="admin/index.html"), name="admin_index_page"),
-    url(r'^login/$', TemplateView.as_view(template_name="account/login.html"), name="user_login_page"),
+    url(r'^login/$', TemplateView.as_view(template_name="oj/account/login.html"), name="user_login_page"),
     url(r'^api/login/$', UserLoginAPIView.as_view(), name="login_api"),
     url(r'^api/login/$', UserLoginAPIView.as_view(), name="user_login_api"),
     url(r'^problem/(?P<problem_id>\d+)/$', "problem.views.problem_page", name="problem_page"),
 
-    url(r'^admin/contest/$', TemplateView.as_view(template_name="admin/add_contest.html"), name="add_contest_page"),
+    url(r'^admin/contest/$', TemplateView.as_view(template_name="admin/contest/add_contest.html"), name="add_contest_page"),
 
 ]
