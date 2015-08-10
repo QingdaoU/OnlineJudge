2752647f586f7efcdf640d46afb07719de5660e3
virusdefender <1670873886@qq.com>
2015-08-10 20:19:35 +0800

    增加判题的测试用例文件

---
+++ judge/tests/c/real_time_timeout.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+
+}
---
+++ judge/tests/c/success.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+    return 0;
+}
