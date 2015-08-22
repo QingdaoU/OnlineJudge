748e36d33c1116803e547763460f723dbba45066
virusdefender <1670873886@qq.com>
2015-08-23 01:47:16 +0800

    删除没用的代码

--- static/src/js/build.js
+++ static/src/js/build.js
@@ -3,47 +3,40 @@
 	baseUrl: "js/",
 	// 第三方脚本模块的别名,jquery比libs/jquery-1.11.1.min.js简洁明了；
     paths: {
-        //百度webuploader
-        webuploader: "lib/webuploader/webuploader",
-
-        jquery: "empty:",
+        jquery: "lib/jquery/jquery",
         avalon: "lib/avalon/avalon",
         editor: "utils/editor",
         uploader: "utils/uploader",
-        validation: "utils/validation",
-        code_mirror: "utils/code_mirror",
-        login: "app/oj/account/login",
-        "bs_alert": "utils/bs_alert",
-        submit_code: "app/oj/problem/submit_code",
+        formValidation: "utils/formValidation",
+        codeMirror: "utils/codeMirror",
+        bsAlert: "utils/bsAlert",
+        problem: "app/oj/problem/problem",
         contest: "app/admin/contest/contest",
+        csrfToken: "utils/csrfToken",
+        admin: "app/admin/admin",
+        chart: "lib/chart/Chart",
+        tagEditor: "lib/tagEditor/jquery.tag-editor.min",
+        jqueryUI: "lib/jqueryUI/jquery-ui",
+        bootstrap: "lib/bootstrap/bootstrap",
+        datetimePicker: "lib/datetime_picker/bootstrap-datetimepicker.zh-CN",
+        validator: "lib/validator/validator",
 
-        //formValidation 不要在代码中单独使用，而是使用和修改utils/validation
-        base: "lib/formValidation/base",
-        helper: "lib/formValidation/helper",
-        "language/zh_CN": "lib/formValidation/language/zh_CN",
-        "framework/bootstrap": "lib/formValidation/framework/bootstrap",
-        "validator/notEmpty": "lib/formValidation/validator/notEmpty",
-        "validator/stringLength": "lib/formValidation/validator/stringLength",
-        "validator/date": "lib/formValidation/validator/date",
-        "validator/integer": "lib/formValidation/validator/integer",
-        "validator/between": "lib/formValidation/validator/between",
 
-        //富文本编辑器 不要直接使用，而是使用上面的editor
+        // ------ 下面写的都不要直接用，而是使用上面的封装版本 ------
+        //富文本编辑器simditor -> editor
         simditor: "lib/simditor/simditor",
         "simple-module": "lib/simditor/module",
         "simple-hotkeys": "lib/simditor/hotkeys",
         "simple-uploader": "lib/simditor/uploader",
 
-        //code mirroe 代码编辑器
-        _code_mirror: "lib/codeMirror/codemirror",
-        code_mirror_clang: "lib/codeMirror/language/clike",
+        //code mirror 代码编辑器 ->codeMirror
+        _codeMirror: "lib/codeMirror/codemirror",
+        codeMirrorClang: "lib/codeMirror/language/clike",
 
-        //bootstrap
-        bootstrap: "lib/bootstrap/bootstrap",
+        //百度webuploader -> uploader
+        webUploader: "lib/webuploader/webuploader",
 
-        //
-        "_datetimepicker": "lib/datetime_picker/bootstrap-datetimepicker",
-        "datetimepicker": "lib/datetime_picker/bootstrap-datetimepicker.zh-CN"
+        "_datetimePicker": "lib/datetime_picker/bootstrap-datetimepicker"
 
     },
     shim: {
