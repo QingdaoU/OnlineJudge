70c64507ccb5f18c25e0b45156328b7ffcf38f6b
virusdefender <1670873886@qq.com>
2015-08-26 14:25:14 +0800

    修改测试文件

--- judge/judger_controller/settings.py
+++ judge/judger_controller/settings.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# 这个redis 是 celery 使用的，包括存储队列信息还有部分统计信息
 redis_config = {
     "host": "121.42.32.129",
     "port": 6379,
@@ -6,6 +7,7 @@ redis_config = {
 }
 
 
+# 判题的 docker 容器的配置参数
 docker_config = {
     "image_name": " a7673b55d263",
     "docker_path": "docker",
@@ -13,10 +15,13 @@ docker_config = {
 }
 
 
+# 测试用例的路径，是主机上的实际路径
 test_case_dir = "/root/test_case/"
-source_code_dir = "/root/"
+# 源代码路径，也就是 manage.py 所在的实际路径
+source_code_dir = "/root/OnlineJudge/"
 
 
+# 存储提交信息的数据库，是 celery 使用的，与 oj.settings/local_settings 等区分，那是 web 服务器访问的地址
 submission_db = {
     "host": "127.0.0.1",
     "port": 3306,
