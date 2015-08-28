8ae8fd246abf7500982405c253d83541b011e2d4
esp <sxwxs@msn.com>
2015-08-28 17:11:40 +0800

    [后端-前台]修复比赛列表仅显示可参加的比赛的问题,小bug,否则选择查看可参加的比赛有可能显示不可见的比赛

--- contest/views.py
+++ contest/views.py
@@ -348,7 +348,7 @@ def contest_list_page(request, page=1):
     # 筛选我能参加的比赛
     join = request.GET.get("join", None)
     if join:
-        contests = Contest.objects.filter(Q(contest_type__in=[1, 2]) | Q(groups__in=request.user.group_set.all())).\
+        contests = contests.filter(Q(contest_type__in=[1, 2]) | Q(groups__in=request.user.group_set.all())).\
             filter(end_time__gt=datetime.datetime.now(), start_time__lt=datetime.datetime.now())
 
     paginator = Paginator(contests, 20)
