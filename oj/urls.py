cfaac4f5a245ca7504edd51af466c2759ba5345b
esp <sxwxs@msn.com>
2015-08-29 09:34:46 +0800

    [后端]修改我的提交列表样式,方便筛选

--- oj/urls.py
+++ oj/urls.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 from django.conf.urls import include, url
-from django.contrib import admin
 from django.views.generic import TemplateView
 
 from account.views import (UserLoginAPIView, UsernameCheckAPIView, UserRegisterAPIView,
                            UserChangePasswordAPIView, EmailCheckAPIView,
                            UserAdminAPIView, UserInfoAPIView)
+
 from announcement.views import AnnouncementAdminAPIView
 
 from contest.views import ContestAdminAPIView, ContestProblemAdminAPIView, ContestPasswordVerifyAPIView
