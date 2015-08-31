883f3d5ecb5391d048957c81a59463f0491f1684
esp <sxwxs@msn.com>
2015-08-31 09:00:13 +0800

    [修复]去掉urls中重复的匹配项

--- oj/urls.py
+++ oj/urls.py
@@ -89,7 +89,6 @@ urlpatterns = [
 
 
     url(r'^problem/(?P<problem_id>\d+)/$', "problem.views.problem_page", name="problem_page"),
-    url(r'^problem/(?P<problem_id>\d+)/$', "problem.views.problem_page", name="problem_page"),
     url(r'^problems/$', "problem.views.problem_list_page", name="problem_list_page"),
     url(r'^problems/(?P<page>\d+)/$', "problem.views.problem_list_page", name="problem_list_page"),
     url(r'^problem/(?P<problem_id>\d+)/submissions/$', "submission.views.problem_my_submissions_list_page",
