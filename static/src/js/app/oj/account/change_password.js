0e322da9d6e37e7fa5ed95d6fc1c7128d62b0837
esp <sxwxs@msn.com>
2015-09-01 19:04:07 +0800

    [前台]修改修改密码功能,api添加@login_requrie,去掉界面和api中的username字段,这个从request可以拿到,要用户输入反而不好弄了,也没必要,顺便改了测试[CI不能SKIP]

--- static/src/js/app/oj/account/change_password.js
+++ static/src/js/app/oj/account/change_password.js
@@ -2,13 +2,12 @@ require(["jquery", "bsAlert", "csrfToken", "validator"], function ($, bsAlert, c
 
     $('form').validator().on('submit', function (e) {
         e.preventDefault();
-        var username = $("#username").val();
         var newPassword = $("#new_password ").val();
         var password = $("#password").val();
         $.ajax({
             beforeSend: csrfTokenHeader,
             url: "/api/change_password/",
-            data: {username: username, new_password: newPassword, old_password: password},
+            data: {new_password: newPassword, old_password: password},
             dataType: "json",
             method: "post",
             success: function (data) {
