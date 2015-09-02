655b18421edd304c2c8c5a47f998356e80d8c3d6
esp <sxwxs@msn.com>
2015-09-02 22:26:58 +0800

    这是为了能以root身份运行celery[CI SKIP]

--- judge/judger_controller/celery.py
+++ judge/judger_controller/celery.py
@@ -5,3 +5,5 @@ from .settings import redis_config
 
 app = Celery("judge", broker='redis://%s:%s/%s' % (redis_config["host"], redis_config["port"], redis_config["db"]),
              include=["judge.judger_controller.tasks"])
+			 
+platforms.C_FORCE_ROOT =True
