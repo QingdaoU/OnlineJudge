eb376c49fc2ee7e166cf8e4103a373ea5f432695
lsxwxs <lsxwxs@aliyun.com>
2015-09-02 22:59:43 +0800

    漏了一个import[CI SKIP]

--- judge/judger_controller/celery.py
+++ judge/judger_controller/celery.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from __future__ import absolute_import
-from celery import Celery
+from celery import Celery, platforms
 from .settings import redis_config
 
 app = Celery("judge", broker='redis://%s:%s/%s' % (redis_config["host"], redis_config["port"], redis_config["db"]),
