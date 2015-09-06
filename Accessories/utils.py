6a8e327e80a9cbe69b3c0d8b1e5349f46bbcdd9c
sxw@401 <sxwxs@msn.com>
2015-09-06 08:11:06 +0800

    新建了附件文件夹,里边放一些oj的管理工具,也可能是以后的正式的后台工具

---
+++ Accessories/utils.py
@@ -0,0 +1,9 @@
+import django
+from contest.models import Contest
+django.setup()
+
+def add_exist_problem_to_contest(problems, contest_id):
+    for problem in problems:
+        print problem.title
+        
+    return
