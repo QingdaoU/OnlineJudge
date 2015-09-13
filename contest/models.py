66cef75bed231c5cf3c1d4fb2729e8fd5ccb5698
sxw@401 <sxwxs@msn.com>
2015-09-13 19:49:59 +0800

    添加第一个ac者的记录

--- contest/models.py
+++ contest/models.py
@@ -94,6 +94,8 @@ class ContestSubmission(models.Model):
     ac_time = models.IntegerField(default=0)
     # 总的时间，用于acm 类型的，也需要保存罚时
     total_time = models.IntegerField(default=0)
+    # 第一个解出此题目
+    first_achieved = models.BooleanField()
 
     class Meta:
         db_table = "contest_submission"
