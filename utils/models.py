c26fd6734d1dd9f249582c38acf0a79ad3075f98
virusdefender <1670873886@qq.com>
2015-09-22 17:03:53 +0800

    增加富文本的 xss 过滤

---
+++ utils/models.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from django.db import models
+
+from utils.xss_filter import XssHtml
+
+
+class RichTextField(models.TextField):
+    __metaclass__ = models.SubfieldBase
+
+    def get_prep_value(self, value):
+        parser = XssHtml()
+        parser.feed(value)
+        parser.close()
+        return parser.getHtml()
