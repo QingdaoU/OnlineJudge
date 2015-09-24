82e8ce3ea8e5f00118904189323882e59a23b344
virusdefender <1670873886@qq.com>
2015-09-24 15:56:47 +0800

    更新用户提交 ac 标志存储方法

--- utils/templatetags/problem.py
+++ utils/templatetags/problem.py
@@ -9,12 +9,16 @@ def get_problem_accepted_radio(problem):
 
 
 def get_problem_status(problems_status, problem_id):
+    # 用户没登陆 或者 user.problem_status 中没有这个字段都会到导致这里的problem_status 为 ""
+    if not problems_status:
+        return ""
 
     if str(problem_id) in problems_status:
         if problems_status[str(problem_id)] == 1:
             return "glyphicon glyphicon-ok ac-flag"
         return "glyphicon glyphicon-minus dealing-flag"
     return ""
+
 
 from django import template
 
