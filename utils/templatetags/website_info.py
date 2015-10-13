306c4ccce913cce1a4780d036431bb370568b81d
virusdefender <1670873886@qq.com>
2015-10-13 09:03:53 +0800

    自定义网站信息

--- utils/templatetags/website_info.py
+++ utils/templatetags/website_info.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 from django import template
+from django.conf import settings
 register = template.Library()
 
 
 @register.simple_tag
 def show_website_info(name):
-    return {"website_name": "qduoj", "website_footer": u"青岛大学信息工程学院 创新实验室"}[name]
+    return settings.WEBSITE_INFO[name]
