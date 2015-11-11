2993a6d7945a0ce649afd994e7eda48b2df1cc7e
virusdefender <1670873886@qq.com>
2015-11-11 21:05:06 +0800

    格式化代码

--- static/src/js/app/oj/account/avatar.js
+++ static/src/js/app/oj/account/avatar.js
@@ -1,5 +1,6 @@
 require(["jquery", "bsAlert", "csrfToken"], function ($, bsAlert, csrfTokenHeader) {
-    var avatar="";
+    var avatar = "";
+
     function changeAvatar(event) {
         avatar = $(event.target).attr('src');
         $('#current_avatar').attr('src', avatar);
@@ -7,27 +8,27 @@ require(["jquery", "bsAlert", "csrfToken"], function ($, bsAlert, csrfTokenHeade
 
     $('.avatar-item').click(changeAvatar);
 
-    $('#save_avatar').click(function(){
+    $('#save_avatar').click(function () {
         if (avatar)
             $.ajax({
-                    beforeSend: csrfTokenHeader,
-                    url: "/api/account/userprofile/",
-                    data: {
-                        avatar: avatar
-                    },
-                    dataType: "json",
-                    method: "put",
-                    success: function (data) {
-                        if (!data.code) {
-                            bsAlert("已保存!");
-                        }
-                        else{
-                            bsAlert(data.data);
-                        }
-                    },
-                    error: function () {
-                        bsAlert("额 好像出错了，请刷新页面重试。如还有问题，请填写页面导航栏上的反馈。")
+                beforeSend: csrfTokenHeader,
+                url: "/api/account/userprofile/",
+                data: {
+                    avatar: avatar
+                },
+                dataType: "json",
+                method: "put",
+                success: function (data) {
+                    if (!data.code) {
+                        bsAlert("已保存!");
                     }
+                    else {
+                        bsAlert(data.data);
+                    }
+                },
+                error: function () {
+                    bsAlert("额 好像出错了，请刷新页面重试。如还有问题，请填写页面导航栏上的反馈。")
+                }
 
             });
         else
