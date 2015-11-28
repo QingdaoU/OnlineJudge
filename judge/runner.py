236102b6accc56a80df1e81b03429c043a8446c3
virusdefender <1670873886@qq.com>
2015-11-29 01:24:15 +0800

    判题结果中返回 server 信息，便于统计

--- judge/runner.py
+++ judge/runner.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import os
+import socket
 import shutil
 
 from client import JudgeClient
@@ -15,7 +16,7 @@ class JudgeInstanceRunner(object):
 
     def run(self, submission_id, language_code, code, time_limit, memory_limit, test_case_id):
         language = languages[language_code]
-
+        host_name = socket.gethostname()
         judge_base_path = os.path.join(judger_workspace, "run", submission_id)
 
         try:
@@ -28,14 +29,14 @@ class JudgeInstanceRunner(object):
             f.close()
         except Exception as e:
             shutil.rmtree(judge_base_path, ignore_errors=True)
-            return {"code": 2, "data": str(e)}
+            return {"code": 2, "data": {"error": str(e), "server": host_name}}
 
         # 编译
         try:
             exe_path = compile_(language, src_path, judge_base_path)
         except Exception as e:
             shutil.rmtree(judge_base_path, ignore_errors=True)
-            return {"code": 1, "data": str(e)}
+            return {"code": 1, "data": {"error": str(e), "server": host_name}}
 
         # 运行
         try:
@@ -45,7 +46,8 @@ class JudgeInstanceRunner(object):
                                  max_real_time=int(time_limit) * 2,
                                  max_memory=int(memory_limit),
                                  test_case_dir=judger_workspace + "test_case/" + test_case_id + "/")
-            judge_result = {"result": result["accepted"], "info": client.run(), "accepted_answer_time": None}
+            judge_result = {"result": result["accepted"], "info": client.run(),
+                            "accepted_answer_time": None, "server": host_name}
 
             for item in judge_result["info"]:
                 if item["result"]:
@@ -56,6 +58,6 @@ class JudgeInstanceRunner(object):
                 judge_result["accepted_answer_time"] = l[-1]["cpu_time"]
             return {"code": 0, "data": judge_result}
         except Exception as e:
-            return {"code": 2, "data": str(e)}
+            return {"code": 2, "data": {"error": str(e), "server": host_name}}
         finally:
             shutil.rmtree(judge_base_path, ignore_errors=True)
