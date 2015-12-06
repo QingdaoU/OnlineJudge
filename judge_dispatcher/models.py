b542d7c5c512d6b3daff8ac8762d015626b8e668
virusdefender <1670873886@qq.com>
2015-12-06 18:45:53 +0800

    增加判题服务器认证 token和锁；增加判题等待队列

--- judge_dispatcher/models.py
+++ judge_dispatcher/models.py
@@ -8,8 +8,19 @@ class JudgeServer(models.Model):
     # 这个服务器最大可能运行的判题实例数量
     max_instance_number = models.IntegerField()
     left_instance_number = models.IntegerField()
+    token = models.CharField(max_length=30)
+    # 进行测试用例同步的时候加锁
+    lock = models.BooleanField(default=False)
     # status 为 false 的时候代表不使用这个服务器
     status = models.BooleanField(default=True)
 
     class Meta:
         db_table = "judge_server"
+
+
+class JudgeWaitingQueue(models.Model):
+    submission_id = models.CharField(max_length=40)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "judge_waiting_queue"
