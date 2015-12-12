84d390362bafc8a369cfa917b6ed43eabaddb610
virusdefender <1670873886@qq.com>
2015-12-12 11:36:02 +0800

    修复 huey 队列不会释放数据库连接的问题，是用法不对

--- submission/tasks.py
+++ submission/tasks.py
@@ -1,9 +1,9 @@
 # coding=utf-8
-from huey.djhuey import task
+from huey.djhuey import db_task
 
 from judge_dispatcher.tasks import JudgeDispatcher
 
 
-@task()
+@db_task()
 def _judge(submission, time_limit, memory_limit, test_case_id, is_waiting_task=False):
     JudgeDispatcher(submission, time_limit, memory_limit, test_case_id).judge(is_waiting_task)
