3d0689eff65be9daf91611dc832f903cafb8052e
virusdefender <1670873886@qq.com>
2016-04-03 13:15:05 +0800

    增加smtp密码和网站名称设置说明

--- oj/custom_settings.example.py
+++ oj/custom_settings.example.py
@@ -4,14 +4,14 @@ import os
 
 WEBSITE_INFO = {"website_name": u"example大学 OnlineJudge",
                 "website_name_shortcut": u"example oj",
-                "website_footer": u"example大学信息学院<a href=\"http://www.miibeian.gov.cn/\">京ICP备xxxxx号-1</a>",
+                "website_footer": u"example大学foobar学院 <a href=\"http://www.miibeian.gov.cn/\">京ICP备10086号-1</a>",
                 # url结尾没有/
-                "url": u"https://your-domain-or-ip.com"}
+                "url": u"http://your-domain-or-ip.com"}
 
 
-SMTP_CONFIG = {"smtp_server": "smtp.xxx.com",
-               "email": "noreply@xxx.com",
-               "password": os.environ.get("smtp_password", "111111"),
+SMTP_CONFIG = {"smtp_server": "smtp.domain.com",
+               "email": "noreply@domain.com",
+               "password": "your_password",
                "tls": False}
 
 # please set your own SECRET_KEY to a long random string
