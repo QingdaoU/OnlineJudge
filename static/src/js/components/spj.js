7414c80ee6c3737e914c407f02dc1a530f235989
virusdefender <1670873886@qq.com>
2016-04-05 20:19:37 +0800

    增加admin页面SPJ的帮助链接

--- static/src/js/components/spj.js
+++ static/src/js/components/spj.js
@@ -5,7 +5,7 @@ define("spj", ["avalon", "bsAlert"], function (avalon, bsAlert) {
         '<div class="form-group">' +
         '<label class="text"><input type="checkbox" ms-duplex-checked="spj" ms-attr-disabled="checkboxDisabled">' +
         '<small> Special Judge用于答案不唯一的情况,需要自己上传判题代码。上传测试用后如需要修改, 必须重新上传对应类型的新测试用例。' +
-        '<a href="#" target="_blank">帮助和示例</a></small>' +
+        '<a href="https://github.com/QingdaoU/OnlineJudge/wiki/SpecialJudge" target="_blank">帮助和示例</a></small>' +
         '</label></div></div>' +
         '<div class="col-md-6" ms-if="spj">' +
         '<label>SPJ代码语言</label>' +
