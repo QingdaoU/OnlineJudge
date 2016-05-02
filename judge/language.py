9f8202ffdcb5f39a3be09ede62eadd7996d49699
virusdefender <qduliyang@outlook.com>
2016-05-02 21:02:01 +0800

    编译器资源限制放入配置文件中

--- judge/language.py
+++ judge/language.py
@@ -6,6 +6,8 @@ languages = {
         "name": "c",
         "src_name": "main.c",
         "code": 1,
+        "compile_max_cpu_time": 3000,
+        "compile_max_memory": 128 * 1024 * 1024,
         "compile_command": "/usr/bin/gcc -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c99 {src_path} -lm -o {exe_path}/main",
         "spj_compile_command": "/usr/bin/gcc -DONLINE_JUDGE -O2 -Werror -fmax-errors=3 -std=c99 {src_path} -lm -o {exe_path}",
         "execute_command": "{exe_path}/main",
@@ -15,6 +17,8 @@ languages = {
         "name": "cpp",
         "src_name": "main.cpp",
         "code": 2,
+        "compile_max_cpu_time": 3000,
+        "compile_max_memory": 128 * 1024 * 1024,
         "compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -w -fmax-errors=3 -std=c++11 {src_path} -lm -o {exe_path}/main",
         "spj_compile_command": "/usr/bin/g++ -DONLINE_JUDGE -O2 -Werror -fmax-errors=3 -std=c++11 {src_path} -lm -o {exe_path}",
         "execute_command": "{exe_path}/main",
@@ -24,6 +28,8 @@ languages = {
         "name": "java",
         "src_name": "Main.java",
         "code": 3,
+        "compile_max_cpu_time": 3000,
+        "compile_max_memory": 1024 * 1024 * 1024,
         "compile_command": "/usr/bin/javac {src_path} -d {exe_path} -encoding UTF8",
         "execute_command": "/usr/bin/java -cp {exe_path} -Xss1M -XX:MaxPermSize=16M "
                            "-XX:PermSize=8M -Xms16M -Xmx{max_memory} -Djava.security.manager "
