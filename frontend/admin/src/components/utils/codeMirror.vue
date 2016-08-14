b5f189d3526731240eb8e63ff9e1e1601c0985c6
virusdefender <qduliyang@outlok.com>
2016-08-14 19:06:50 +0800

    增加CodeMirror组件

---
+++ frontend/admin/src/components/utils/codeMirror.vue
@@ -0,0 +1,45 @@
+<template>
+    <textarea id="{{ editorId }}"></textarea>
+</template>
+
+<script>
+    import CodeMirror from "codemirror"
+    import "codemirror/mode/javascript/javascript"
+    import "codemirror/mode/python/python"
+
+    export default({
+        data() {
+            return {
+                editor: {},
+                editorId: Math.random().toString(36).substr(2),
+                language: "python"
+            }
+        },
+        attached() {
+            CodeMirror.fromTextArea(document.getElementById(this.editorId), {
+                lineNumbers: true,
+                mode: this.language,
+                indentUnit: 4,
+                matchBrackets: true
+            });
+        }
+    })
+</script>
+
+<style>
+    @import "../../../../static/css/CodeMirror.css";
+
+    .CodeMirror {
+        min-height: 250px;
+        _height: 250px;
+        height: auto;
+    }
+
+    .CodeMirror-scroll {
+        overflow: auto;
+        min-height: 250px;
+        height: auto;
+        position: relative;
+        outline: none;
+    }
+</style>
