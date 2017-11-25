7fce29cb717b0b758bf090a7d93c9b99cfe9562c
virusdefender <virusdefender@outlook.com>
2017-11-25 15:47:56 +0800

    fix static file path

--- oj/settings.py
+++ oj/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/storage/'
+STATIC_URL = '/public/'
 
 AUTH_USER_MODEL = 'account.User'
 
