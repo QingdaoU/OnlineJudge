cc857e65d9496cdb2e2014cebbc77719d9554d42
zema1 <rawidn@163.com>
2017-12-04 15:29:55 +0800

    add migrate script

--- deploy/health_check.py
+++ deploy/health_check.py
@@ -2,7 +2,7 @@ import xmlrpclib
 
 if __name__ == "__main__":
     try:
-        server = xmlrpclib.Server('http://localhost:9005/RPC2')
+        server = xmlrpclib.Server("http://localhost:9005/RPC2")
         info = server.supervisor.getAllProcessInfo()
         error_states = list(filter(lambda x: x["state"] != 20, info))
         exit(len(error_states))
