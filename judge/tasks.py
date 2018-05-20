a3ee2ea21218e802492de5432b88aa346b39df66
virusdefender <virusdefender@outlook.com>
2018-05-20 08:20:17 +0800

    忽略被封禁用户的提交

--- judge/tasks.py
+++ judge/tasks.py
@@ -1,8 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+
+from account.models import User
+from submission.models import Submission
 from judge.dispatcher import JudgeDispatcher
 
 
 @shared_task
 def judge_task(submission_id, problem_id):
+    uid = Submission.objects.get(id=submission_id).user_id
+    if User.objects.get(id=uid).is_disabled:
+        return
     JudgeDispatcher(submission_id, problem_id).judge()
