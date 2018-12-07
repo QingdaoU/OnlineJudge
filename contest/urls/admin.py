7d0d1879edf755708f54a30f1184e3e2b7f8d851
HandsomeHow <673712786@qq.com>
2018-12-07 20:24:40 +0800

    fixed spell error

--- contest/urls/admin.py
+++ contest/urls/admin.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 
-from ..views.admin import ContestAnnouncementAPI, ContestAPI, ACMContestHelper, DownloadContestSubmissions, ContestCheckSimiliarAPI
+from ..views.admin import ContestAnnouncementAPI, ContestAPI, ACMContestHelper, DownloadContestSubmissions, ContestCheckSimilarAPI
 
 urlpatterns = [
     url(r"^contest/?$", ContestAPI.as_view(), name="contest_admin_api"),
     url(r"^contest/announcement/?$", ContestAnnouncementAPI.as_view(), name="contest_announcement_admin_api"),
     url(r"^contest/acm_helper/?$", ACMContestHelper.as_view(), name="acm_contest_helper"),
     url(r"^download_submissions/?$", DownloadContestSubmissions.as_view(), name="acm_contest_helper"),
-    url(r"^contesteck_similiar/?$", ContestCheckSimiliarAPI.as_view(), name="contest_check_similiar_admin_api")
+    url(r"^contest_check_similar/?$", ContestCheckSimilarAPI.as_view(), name="contest_check_similar_admin_api")
 ]
