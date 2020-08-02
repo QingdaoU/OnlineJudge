cd3c299b5aac7ce28bbf191b09fab552edf7832c
Harry-zklcdc <1269158832@qq.com>
2020-08-02 14:59:12 +0800

    [Add] Experience Rank

--- account/views/oj.py
+++ account/views/oj.py
@@ -385,8 +385,10 @@ class UserRankAPI(APIView):
             .select_related("user")
         if rule_type == ContestRuleType.ACM:
             profiles = profiles.filter(submission_number__gt=0).order_by("-accepted_number", "submission_number")
-        else:
+        elif rule_type == ContestRuleType.OI:
             profiles = profiles.filter(total_score__gt=0).order_by("-total_score")
+        else:
+            profiles = profiles.filter(experience__gt=0).order_by("-experience")
         data = self.paginate_data(request, profiles, RankInfoSerializer)
         for i in range(0, len(data["results"])):
             user = User.objects.get(username=data["results"][i]["user"]["username"], is_disabled=False)
