e009777c6f67d86c504144b01bdd290357536418
helsonxiao <helsonxiao@gmail.com>
2022-05-08 17:13:22 +0800

    fix: rundramatiq

--- oj/dev_settings.py
+++ oj/dev_settings.py
@@ -1,22 +1,23 @@
 # coding=utf-8
 import os
+from utils.shortcuts import get_env
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '127.0.0.1',
-        'PORT': 5435,
-        'NAME': "onlinejudge",
-        'USER': "onlinejudge",
-        'PASSWORD': 'onlinejudge'
+        'HOST': get_env('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': get_env('POSTGRES_PORT', '5435'),
+        'NAME': get_env('POSTGRES_DB', 'onlinejudge'),
+        'USER': get_env('POSTGRES_USER', 'onlinejudge'),
+        'PASSWORD': get_env('POSTGRES_PASSWORD', 'onlinejudge')
     }
 }
 
 REDIS_CONF = {
-    "host": "127.0.0.1",
-    "port": "6380"
+    'host': get_env('REDIS_HOST', '127.0.0.1'),
+    'port': get_env('REDIS_PORT', '6380')
 }
 
 
